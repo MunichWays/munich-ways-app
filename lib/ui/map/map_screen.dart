0c179f15b61db3dc0e5488d3e66a080f9bb98d39
Sven Adolph <mail@svenadolph.net>
2020-09-25 14:48:23 +0200

    Add current location button to map

--- lib/ui/map/map_screen.dart
+++ lib/ui/map/map_screen.dart
@@ -1,5 +1,7 @@
+import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:munich_ways/common/logger_setup.dart';
 import 'package:munich_ways/ui/map/map_screen_model.dart';
 import 'package:munich_ways/ui/side_drawer.dart';
 import 'package:provider/provider.dart';
@@ -11,139 +13,244 @@ class MapScreen extends StatefulWidget {
   _MapScreenState createState() => _MapScreenState();
 }
 
-class _MapScreenState extends State<MapScreen> {
-  static const STACHUS = [48.14, 11.5652];
+class _MapScreenState extends State<MapScreen> with WidgetsBindingObserver {
+  final LatLng _stachus = LatLng(48.14, 11.5652);
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
-  GoogleMapController mapController;
-  final LatLng _center = LatLng(STACHUS[0], STACHUS[1]);
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
 
-  void initState() {}
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    log.d("didChangeAppLifecycleState $state");
+    if(displayCurrentLocationOnResume && state == AppLifecycleState.resumed) {
+      displayCurrentLocationOnResume = false;
+      mapViewModel.displayCurrentLocation(permissionCheck: false);
+    }
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  Future<void> _askForLocationPermission() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Standortberechtigung'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                    'Zur Anzeige des aktuellen Standorts benötigt die App die Berechtigung "Standort".'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Abbrechen'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('Appeinstellungen'),
+              onPressed: () {
+                AppSettings.openAppSettings();
+                displayCurrentLocationOnResume = true;
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  bool displayCurrentLocationOnResume = false;
+  MapScreenViewModel mapViewModel;
+
+
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<MapScreenViewModel>(
       create: (BuildContext context) {
-        return MapScreenViewModel();
+        MapScreenViewModel model = MapScreenViewModel();
+        mapViewModel = model;
+        model.errorMsgs.listen((errorMsg) async {
+          scaffoldKey.currentState.hideCurrentSnackBar();
+          scaffoldKey.currentState.showSnackBar(SnackBar(
+            content: Text(errorMsg),
+            duration: Duration(seconds: 2),
+          ));
+        });
+        model.showLocationPermissionDialog.listen((_) {
+          _askForLocationPermission();
+        });
+        return model;
       },
       child: Consumer<MapScreenViewModel>(
         builder: (context, model, child) {
           return Scaffold(
+            key: scaffoldKey,
             drawer: SideDrawer(),
-            body: SafeArea(
-              child: Stack(
-                children: [
-                  GoogleMap(
-                    polylines: model.polylines,
-                    myLocationButtonEnabled: true,
-                    myLocationEnabled: false,
-                    onMapCreated: _onMapCreated,
-                    initialCameraPosition: CameraPosition(
-                      target: _center,
-                      zoom: 14.0,
-                    ),
-                    mapType: MapType.normal,
-                    mapToolbarEnabled: true,
-                    zoomControlsEnabled: false,
+            body: Stack(
+              children: [
+                GoogleMap(
+                  polylines: model.polylines,
+                  myLocationButtonEnabled: false,
+                  myLocationEnabled: model.currentLocationVisible,
+                  onMapCreated: (controller) {
+                    model.onMapCreated(controller);
+                  },
+                  initialCameraPosition: CameraPosition(
+                    target: _stachus,
+                    zoom: 14.0,
                   ),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: RawMaterialButton(
-                        onPressed: () {
-                          showModalBottomSheet<void>(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return StatefulBuilder(
-                                builder: (context, StateSetter setModalState) {
-                                  return Container(
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      mainAxisSize: MainAxisSize.min,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.end,
-                                      children: <Widget>[
-                                        Padding(
-                                          padding: const EdgeInsets.fromLTRB(16.0, 8, 16, 0),
-                                          child: Row(
-                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Text(
-                                                "Fahrradnetz auswählen",
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .headline6,
-                                              ),
-                                              IconButton(
-                                                icon: Icon(Icons.close),
-                                                onPressed: () =>
-                                                    Navigator.pop(context),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        CheckboxListTile(
-                                          title: Text("Radvorrangnetz"),
-                                          value: model.isRadlvorrangnetzVisible,
-                                          onChanged: (bool value) {
-                                            model.toggleRadvorrangnetzVisible();
-                                            setModalState(() {});
-                                          },
-                                        ),
-                                        CheckboxListTile(
-                                          title: Text("Gesamtnetz"),
-                                          value: model.isGesamtnetzVisible,
-                                          onChanged: (bool value) {
-                                            model.toggleGesamtnetzVisible();
-                                            setModalState(() {});
-                                          },
-                                        ),
-                                      ],
-                                    ),
+                  mapType: MapType.normal,
+                  mapToolbarEnabled: true,
+                  zoomControlsEnabled: false,
+                  compassEnabled: true,
+                  padding: EdgeInsets.only(top: 88),
+                ),
+                SafeArea(
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              RawMaterialButton(
+                                onPressed: () {
+                                  showModalBottomSheet<void>(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return StatefulBuilder(
+                                        builder: (context,
+                                            StateSetter setModalState) {
+                                          return Container(
+                                            child: Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              mainAxisSize: MainAxisSize.min,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.end,
+                                              children: <Widget>[
+                                                Padding(
+                                                  padding:
+                                                      const EdgeInsets.fromLTRB(
+                                                          16.0, 8, 16, 0),
+                                                  child: Row(
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment
+                                                            .spaceBetween,
+                                                    children: [
+                                                      Text(
+                                                        "Fahrradnetz auswählen",
+                                                        style: Theme.of(context)
+                                                            .textTheme
+                                                            .headline6,
+                                                      ),
+                                                      IconButton(
+                                                        icon: Icon(Icons.close),
+                                                        onPressed: () =>
+                                                            Navigator.pop(
+                                                                context),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                                CheckboxListTile(
+                                                  title: Text("Radvorrangnetz"),
+                                                  value: model
+                                                      .isRadlvorrangnetzVisible,
+                                                  onChanged: (bool value) {
+                                                    model
+                                                        .toggleRadvorrangnetzVisible();
+                                                    setModalState(() {});
+                                                  },
+                                                ),
+                                                CheckboxListTile(
+                                                  title: Text("Gesamtnetz"),
+                                                  value:
+                                                      model.isGesamtnetzVisible,
+                                                  onChanged: (bool value) {
+                                                    model
+                                                        .toggleGesamtnetzVisible();
+                                                    setModalState(() {});
+                                                  },
+                                                ),
+                                              ],
+                                            ),
+                                          );
+                                        },
+                                      );
+                                    },
                                   );
                                 },
-                              );
-                            },
-                          );
-                        },
-                        elevation: 2.0,
-                        fillColor: Colors.white,
-                        constraints:
-                            BoxConstraints.expand(width: 56, height: 56),
-                        child: Icon(
-                          Icons.layers,
-                          color: Colors.black54,
+                                elevation: 2.0,
+                                fillColor: Colors.white,
+                                constraints: BoxConstraints.expand(
+                                    width: 56, height: 56),
+                                child: Icon(
+                                  Icons.layers,
+                                  color: Colors.black54,
+                                ),
+                                padding: EdgeInsets.all(15.0),
+                                shape: CircleBorder(),
+                              ),
+                              SizedBox(
+                                height: 16,
+                              ),
+                              RawMaterialButton(
+                                onPressed: () async {
+                                  model.displayCurrentLocation();
+                                },
+                                elevation: 2.0,
+                                fillColor: Colors.white,
+                                constraints: BoxConstraints.expand(
+                                    width: 56, height: 56),
+                                child: Icon(
+                                  model.currentLocationVisible ? Icons.my_location : Icons.location_searching,
+                                  color: model.currentLocationVisible ? Colors.black54 : Colors.black26,
+                                ),
+                                padding: EdgeInsets.all(15.0),
+                                shape: CircleBorder(),
+                              ),
+                            ],
+                          ),
                         ),
-                        padding: EdgeInsets.all(15.0),
-                        shape: CircleBorder(),
                       ),
-                    ),
-                  ),
-                  MapAppBar(
-                    actions: <Widget>[
-                      IconButton(
-                        icon: const Icon(Icons.info_outline),
-                        tooltip: 'Legende',
-                        onPressed: () {
-                          //TODO display legende or infos about munichways map
-                          setState(() {});
-                        },
+                      MapAppBar(
+                        actions: <Widget>[
+                          IconButton(
+                            icon: const Icon(Icons.info_outline),
+                            tooltip: 'Legende',
+                            onPressed: () {
+                              //TODO display legende or infos about munichways map
+                            },
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           );
         },
       ),
     );
-  }
-
-  void _onMapCreated(GoogleMapController controller) {
-    setState(() {
-      mapController = controller;
-    });
   }
 }
--- lib/ui/map/map_screen_model.dart
+++ lib/ui/map/map_screen_model.dart
@@ -1,16 +1,21 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:munich_ways/common/logger_setup.dart';
 import 'package:munich_ways/ui/map/munichways_api.dart';
 
 class MapScreenViewModel extends ChangeNotifier {
+  GoogleMapController mapController;
+
   Set<Polyline> get polylines {
     Set<Polyline> tempPolylines = {};
-    if(_isRadlvorrangnetzVisible){
-      tempPolylines.addAll(_polylines_vorrangnetz);
+    if (_isRadlvorrangnetzVisible) {
+      tempPolylines.addAll(_polylinesVorrangnetz);
     }
-    if(_isGesamtnetzVisible){
-      tempPolylines.addAll(_polylines_gesamtnetz);
+    if (_isGesamtnetzVisible) {
+      tempPolylines.addAll(_polylinesGesamtnetz);
     }
     return tempPolylines;
   }
@@ -26,35 +31,98 @@ class MapScreenViewModel extends ChangeNotifier {
     return _isGesamtnetzVisible;
   }
 
-  void toggleGesamtnetzVisible(){
-    _isGesamtnetzVisible = !_isGesamtnetzVisible;
-    notifyListeners();
-  }
+  bool currentLocationVisible = false;
 
-  void toggleRadvorrangnetzVisible(){
-    _isRadlvorrangnetzVisible = !_isRadlvorrangnetzVisible;
-    notifyListeners();
-  }
-
-  Set<Polyline> _polylines_vorrangnetz;
-  Set<Polyline> _polylines_gesamtnetz;
+  Set<Polyline> _polylinesVorrangnetz = {};
+  Set<Polyline> _polylinesGesamtnetz = {};
 
   MunichwaysApi netzRepo = MunichwaysApi();
 
+  Stream<String> errorMsgs;
+  StreamController<String> _errorMsgsController;
+
+
+  Stream showLocationPermissionDialog;
+  StreamController _permissionStreamController;
+
   MapScreenViewModel() {
+    _errorMsgsController = StreamController();
+    errorMsgs = _errorMsgsController.stream;
+    _permissionStreamController = StreamController();
+    showLocationPermissionDialog = _permissionStreamController.stream;
+
     refreshVorrangnetz();
     refreshGesamtznetz();
   }
 
+  void _displayErrorMsg(String msg) {
+    _errorMsgsController.add(msg);
+  }
+
+  void onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    displayCurrentLocation();
+  }
+
+  Future<void> displayCurrentLocation({bool permissionCheck = true}) async {
+    LocationPermission permission = await checkPermission();
+    log.d(permission);
+    if (!permissionCheck &&
+        (permission == LocationPermission.denied ||
+            permission == LocationPermission.deniedForever)) {
+      log.d("ignore missing permission");
+      return;
+    }
+    switch (permission) {
+      case LocationPermission.denied:
+        permission = await requestPermission();
+        if (permission == LocationPermission.denied ||
+            permission == LocationPermission.deniedForever) {
+          currentLocationVisible = false;
+          _displayErrorMsg(
+              "Standort Berechtigung fehlt.");
+          return;
+        }
+        break;
+      case LocationPermission.deniedForever:
+        _permissionStreamController.add("");
+        break;
+      case LocationPermission.whileInUse:
+      case LocationPermission.always:
+        currentLocationVisible = true;
+        Position position = await getLastKnownPosition();
+        if (position != null) {
+          position =
+              await getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+        }
+        mapController.animateCamera(CameraUpdate.newCameraPosition(
+            CameraPosition(
+                zoom: await mapController.getZoomLevel(),
+                target: LatLng(position.latitude, position.longitude))));
+        notifyListeners();
+        break;
+    }
+  }
+
+  void toggleGesamtnetzVisible() {
+    _isGesamtnetzVisible = !_isGesamtnetzVisible;
+    notifyListeners();
+  }
+
+  void toggleRadvorrangnetzVisible() {
+    _isRadlvorrangnetzVisible = !_isRadlvorrangnetzVisible;
+    notifyListeners();
+  }
+
   Future<void> refreshVorrangnetz() async {
     log.d('refreshVorrangnetz');
-    _polylines_vorrangnetz = await netzRepo.getRadlvorrangnetz();
+    _polylinesVorrangnetz = await netzRepo.getRadlvorrangnetz();
     notifyListeners();
   }
 
   Future<void> refreshGesamtznetz() async {
     log.d('refreshGesamtnetz');
-    _polylines_gesamtnetz = await netzRepo.getGesamtnetz();
+    _polylinesGesamtnetz = await netzRepo.getGesamtnetz();
     notifyListeners();
   }
 }
