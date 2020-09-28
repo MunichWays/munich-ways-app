a3cfb7be3ce4f946b56af5d3582e24a9454a043f
Sven Adolph <mail@svenadolph.net>
2020-09-28 10:01:26 +0200

    Fix encoding for munichways jsons

---
+++ lib/common/json_body_extension.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+extension JsonBody on Response {
+
+  dynamic jsonBody(){
+    // Use utf8 encoding instead of latin1 which is used by body
+    // see https://github.com/dart-lang/http/issues/367
+    String jsonBody = Utf8Decoder().convert(this.bodyBytes);
+    return json.decode(jsonBody);
+  }
+
+}
