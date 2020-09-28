e5115a699887b43cc06781d30e78e9a3d0c863b5
Sven Adolph <mail@svenadolph.net>
2020-09-28 21:54:40 +0200

    Fix street details test

--- test/street_details_test.dart
+++ test/street_details_test.dart
@@ -17,7 +17,6 @@ void main(){
     StreetDetails details = StreetDetails.fromJson(json);
 
     //THEN
-    // ignore: closing_tag_is_redundant
     expect(details, StreetDetails(
         cartoDbId: 1161,
         name: "Theresienwiese-Lücke-Beethovenstr.-Mattias-Pschorr-Str./Mozartstr.",
@@ -28,7 +27,7 @@ void main(){
         soll: "Fahrbahn erneuern",
         kategorie: "<a href=\"https://github.com/MunichWays/bike-infrastructure/wiki/ebener-Radweg\" target=\"_blank\"> ebener Radweg</a>",
         farbe: "schwarz",
-        links: "<!--suppress ALL --><!--suppress HtmlExtraClosingTag --><p><a href=\"https://docs.google.com/document/d/14HlSPaYCMvT2v00sMvJYW4j0Hbq8jHdvr9bl2sZTlbk/edit?usp=sharing\" target=\"_blank\">  RadlVorrangProfil</a></p>\n<p>_____ </p><br> </br>\n<p><a href=\"https://docs.google.com/document/d/11cXxckedsvTK9OPncjArQdBjZ_gmi_mKEtMENqmjYkE/edit?usp=sharing\" target=\"_blank\">  Maßnahmenkatalog </a> </p>\n<p><a href=\"https://www.ris-muenchen.de/RII/RII/DOK/ANTRAG/5645187.pdf\" target=\"_blank\">  RIS Antrag SPD 19.09.2019 </a> </p>\n<p>_____ </p><br> </br>",
+        links: "<p><a href=\"https://docs.google.com/document/d/14HlSPaYCMvT2v00sMvJYW4j0Hbq8jHdvr9bl2sZTlbk/edit?usp=sharing\" target=\"_blank\">  RadlVorrangProfil</a></p>\n<p>_____ </p><br> </br>\n<p><a href=\"https://docs.google.com/document/d/11cXxckedsvTK9OPncjArQdBjZ_gmi_mKEtMENqmjYkE/edit?usp=sharing\" target=\"_blank\">  Maßnahmenkatalog </a> </p>\n<p><a href=\"https://www.ris-muenchen.de/RII/RII/DOK/ANTRAG/5645187.pdf\" target=\"_blank\">  RIS Antrag SPD 19.09.2019 </a> </p>\n<p>_____ </p><br> </br>",
         strecke: "RV3S - Fürstenrieder-Radlroute",
         quartal: "Q4_2020",
         bild: "<a href=\"https://www.mapillary.com/map/im/MoDbKpzUOr9tcd5Gx22gyE\" target=\"_blank\">\n<img src=\"https://www.munichways.com/img/Theresienwiese_175.jpg\" width=175></a>",
