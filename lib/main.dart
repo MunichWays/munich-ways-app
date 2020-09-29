eefd57d4f3904e0775e7ab43270a9424ebb496c7
Sven Adolph <mail@svenadolph.net>
2020-09-29 14:54:18 +0200

    Add info screen and adjust navigation drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:munich_ways/info_screen.dart';
 import 'package:munich_ways/nav_routes.dart';
 import 'package:munich_ways/ui/map/map_screen.dart';
+import 'package:munich_ways/ui/theme.dart';
 
 void main() => runApp(MunichWaysApp());
 
@@ -13,11 +15,18 @@ class _MunichWaysAppState extends State<MunichWaysApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: themeData,
       onGenerateRoute: (settings) {
         switch (settings.name) {
           case NavRoutes.root:
           case NavRoutes.map:
-            return MaterialPageRoute(builder: (context) => MapScreen());
+            return MaterialPageRoute(
+                settings: RouteSettings(name: settings.name),
+                builder: (context) => MapScreen());
+          case NavRoutes.info:
+            return MaterialPageRoute(
+                settings: RouteSettings(name: settings.name),
+                builder: (context) => InfoScreen());
           default:
             throw Exception("unknown route ${settings.name}");
         }
--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -3,3 +3,9 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const munichWaysBlue = Color(0xff61A1D8);
 }
+
+var themeData = ThemeData(
+  primaryColor: AppColors.munichWaysBlue,
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+  buttonColor: AppColors.munichWaysBlue,
+);
