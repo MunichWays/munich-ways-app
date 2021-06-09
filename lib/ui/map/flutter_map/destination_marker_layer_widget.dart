66040fdc22b976e49555c3e842bae40b3a9e9409
Sven Adolph <mail@svenadolph.net>
2021-06-09 16:38:51 +0200

    Add destination marker to map #5

---
+++ lib/ui/map/flutter_map/destination_marker_layer_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:munich_ways/model/place.dart';
+
+class DestinationMarkerLayerWidget extends StatelessWidget {
+  final Place destination;
+
+  const DestinationMarkerLayerWidget({Key key, this.destination})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return destination != null
+        ? MarkerLayerWidget(
+            options: MarkerLayerOptions(
+            markers: [
+              Marker(
+                  width: 35.0,
+                  height: 48.0,
+                  point: destination.latLng,
+                  builder: (ctx) => Container(
+                        child: Image(image: AssetImage('images/pin.png')),
+                      ),
+                  rotate: true),
+            ],
+          ))
+        : Container();
+  }
+}
