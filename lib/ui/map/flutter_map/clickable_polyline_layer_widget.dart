3fca2ba4d153b5ccf412f58daf25f05c84824479
Sven Adolph <mail@svenadolph.net>
2022-02-03 22:06:08 +0100

    Fix analyzer issues

--- lib/ui/map/flutter_map/clickable_polyline_layer_widget.dart
+++ lib/ui/map/flutter_map/clickable_polyline_layer_widget.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:munich_ways/common/logger_setup.dart';
 
